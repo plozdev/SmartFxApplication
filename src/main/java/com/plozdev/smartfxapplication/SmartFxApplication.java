3770c1b1396fbf8d9cf3e760cfc2f7e52b47ac9c
plozdev <hoangmai.it.dev@gmail.com>
2026-04-13 12:14:31 +0700

    first init

---
+++ src/main/java/com/plozdev/smartfxapplication/SmartFxApplication.java
@@ -0,0 +1,13 @@
+package com.plozdev.smartfxapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plozdev/smartfxapplication/SmartFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plozdev.smartfxapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
