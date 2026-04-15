886bf879279636baf92cb1c15c793578095595b4
plozdev <hoangmai.it.dev@gmail.com>
2026-04-15 18:13:44 +0700

    refactor: Clean up unused imports
    - FastForexFetchAllResponse: Remove unused @Builder import
    - InfiniteCycleException: Remove @ResponseStatus (handled by GlobalExceptionHandler)
    - Graph: Remove unused Collections, HashSet, Set imports
    
    No functional changes - cleanup only

--- src/main/java/com/plozdev/smartfxapplication/dto/api/FastForexFetchAllResponse.java
+++ src/main/java/com/plozdev/smartfxapplication/dto/api/FastForexFetchAllResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.Map;
--- src/main/java/com/plozdev/smartfxapplication/model/Graph.java
+++ src/main/java/com/plozdev/smartfxapplication/model/Graph.java
@@ -5,12 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
